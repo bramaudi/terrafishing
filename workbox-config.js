e92bdf878087fe598068375b6e15eb89af9544c2
Brama Udi <bramaudi@users.noreply.github.com>
2021-04-20 05:06:20 +0700

    Add service worker

---
+++ workbox-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  globDirectory: './',
+  globPatterns: [
+    '**/*.{html,js,css}'
+  ],
+  swDest: './sw.js',
+
+  runtimeCaching: [{
+    // Match any request that ends with .png, .jpg, .jpeg or .svg.
+    urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+    // Apply a cache-first strategy.
+    handler: 'CacheFirst',
+
+    options: {
+      // Use a custom cache name.
+      cacheName: 'images',
+
+      // Only cache 10 images.
+      expiration: {
+        maxEntries: 10,
+      },
+    },
+  }],
+}
